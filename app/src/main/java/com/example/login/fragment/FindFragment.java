c49ae17333c829aa57cdad9ac089b57c57e6a565
xiaohua <daaoao51@gmail.com>
2024-02-28 14:58:17 +0800

    For test1

---
+++ app/src/main/java/com/example/login/fragment/FindFragment.java
@@ -0,0 +1,53 @@
+package com.example.login.fragment;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.MapsInitializer;
+import com.amap.api.maps.model.MyLocationStyle;
+import com.amap.api.services.core.ServiceSettings;
+import com.example.login.R;
+
+
+public class FindFragment extends Fragment {
+    private View root;
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1001;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        if (root == null){
+            root = inflater.inflate(R.layout.fragment_find, container, false);
+        }
+
+
+        return root;
+    }
+
+
+
+}
